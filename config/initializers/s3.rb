713861bae3a62e5cbe8545144a52485842ce73eb
nxxn <nxxn@inbox.lv>
2011-06-21 15:13:00 +0300

    gemfile changed

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,4 +1,4 @@
 AWS::S3::Base.establish_connection!(
-  :access_key_id     => ENV['S3_KEY'],
-  :secret_access_key => ENV['S3_SECRET']
+  :access_key_id     => ['AKIAIBIBXKBQRTDU7MOQ'],
+  :secret_access_key => ['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']
 )
