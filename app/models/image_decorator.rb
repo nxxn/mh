44cac010207db0e870c3fa7ecdaa5e87a68ceaaf
nxxn <nxxn@inbox.lv>
2011-06-22 13:27:17 +0300

    lol

--- app/models/image_decorator.rb
+++ app/models/image_decorator.rb
@@ -1,5 +1,5 @@
 Image.attachment_definitions[:attachment][:storage] = :s3
-Image.attachment_definitions[:attachment][:s3_credentials] => "#{Rails.root}/config/s3.yml"
+Image.attachment_definitions[:attachment][:s3_credentials] = "#{Rails.root}/config/s3.yml"
 Image.attachment_definitions[:attachment][:bucket] = 'musthave'
 Image.attachment_definitions[:attachment][:path] = "products/:id/:style.:extension"
 Image.attachment_definitions[:attachment][:s3_headers] = {'Cache-Control' => 'max-age=31557600'}
