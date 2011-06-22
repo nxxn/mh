5f366a75cefc078f2e78a1c7b42c55c80245cbb3
nxxn <nxxn@inbox.lv>
2011-06-22 11:59:33 +0300

    lol

--- app/models/image_decorator.rb
+++ app/models/image_decorator.rb
@@ -3,7 +3,7 @@ Image.attachment_definitions[:attachment][:s3_credentials] = {
   :access_key_id => ['AKIAIBIBXKBQRTDU7MOQ'],
   :secret_access_key => ['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']
 }
-Image.attachment_definitions[:attachment][:bucket] = ENV['musthave']
+Image.attachment_definitions[:attachment][:bucket] = ['musthave']
 Image.attachment_definitions[:attachment][:path] = "products/:id/:style.:extension"
 Image.attachment_definitions[:attachment][:s3_headers] = {'Cache-Control' => 'max-age=31557600'}
 Image.attachment_definitions[:attachment].delete :url
