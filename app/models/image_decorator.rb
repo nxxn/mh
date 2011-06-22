948ac5b84bb35a89bd62e26d829db206972a938c
nxxn <nxxn@inbox.lv>
2011-06-22 12:40:50 +0300

    lol

--- app/models/image_decorator.rb
+++ app/models/image_decorator.rb
@@ -1,8 +1,9 @@
 Image.attachment_definitions[:attachment][:storage] = :s3
 Image.attachment_definitions[:attachment][:s3_credentials] = {
- "#{Rails.root}/config/s3.yml"
+  :access_key_id => ['AKIAIBIBXKBQRTDU7MOQ'],
+  :secret_access_key => ['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']
 }
-
+Image.attachment_definitions[:attachment][:bucket] = ['musthave']
 Image.attachment_definitions[:attachment][:path] = "products/:id/:style.:extension"
 Image.attachment_definitions[:attachment][:s3_headers] = {'Cache-Control' => 'max-age=31557600'}
 Image.attachment_definitions[:attachment].delete :url
