e67df5d2a47d7aeb77d90e97b39dcb65250d4f1c
nxxn <nxxn@inbox.lv>
2011-07-26 01:08:10 +0300

    lol

--- site/app/helpers/account_helper.rb
+++ site/app/helpers/account_helper.rb
@@ -1,8 +1,3 @@
 module AccountHelper
-  def seo_url(taxon, product = nil)
-    return '/t/' + taxon.permalink if product.nil?
-    warn "DEPRECATION: the /t/taxon-permalink/p/product-permalink urls are "+
-      "not used anymore. Use product_url instead. (called from #{caller[0]})"
-    return product_url(product)
-  end
+  
 end
