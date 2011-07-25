9b5441bf1ce45ffff75a8f96b0e9efa6d6efbeaf
nxxn <nxxn@inbox.lv>
2011-07-25 23:26:44 +0300

    lol

--- site/app/helpers/trackers_helper.rb
+++ site/app/helpers/trackers_helper.rb
@@ -1,3 +1,8 @@
 module TrackersHelper
- 
+  def seo_url(taxon, product = nil)
+    return '/t/' + taxon.permalink if product.nil?
+    warn "DEPRECATION: the /t/taxon-permalink/p/product-permalink urls are "+
+      "not used anymore. Use product_url instead. (called from #{caller[0]})"
+    return product_url(product)
+  end
 end
