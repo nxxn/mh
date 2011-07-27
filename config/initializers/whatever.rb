791bc0b662dd7e1179868a9974cd75920e9cd457
nxxn <nxxn@inbox.lv>
2011-07-28 01:15:17 +0300

    kol

--- config/initializers/whatever.rb
+++ config/initializers/whatever.rb
@@ -2,4 +2,5 @@ if Spree::Config.instance
 Spree::Config.set(:default_locale => "ru")
 Spree::Config.set(:default_country_id=> "110")
 Spree::Config.set(:address_requires_state=> "false")
+Spree::Config.set(:admin_products_per_page=> "500")
 end
