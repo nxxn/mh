ebc69a6ab24c189f9e135d0ea5b312cba9ae61a4
nxxn <nxxn@inbox.lv>
2011-10-08 21:33:44 +0300

    lol

--- lib/spree_site.rb
+++ lib/spree_site.rb
@@ -4,20 +4,24 @@ module SpreeSite
       Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
         Rails.configuration.cache_classes ? require(c) : load(c)
       end
-    end
-        
-    ActiveSupport::Notifications.subscribe('spree.order.contents_changed') do |*args|
-      event = ActiveSupport::Notifications::Event.new(*args)
-      order = event.payload[:order]
-      order.line_items.each do |line_item|
-        if((order.state == "cart") && (line_item.quantity > line_item.variant.on_hand))
-          line_item.update_attribute(:quantity, line_item.variant.on_hand)
-          #order.update_totals && order.save
-          order.reload.update!
+      
+      ActiveSupport::Notifications.subscribe('spree.order.contents_changed') do |*args|
+        event = ActiveSupport::Notifications::Event.new(*args)
+        order = event.payload[:order]
+        order.line_items.each do |line_item|
+          if((order.state == "cart") && (line_item.quantity > line_item.variant.on_hand))
+            line_item.update_attribute(:quantity, line_item.variant.on_hand)
+            #order.update_totals && order.save
+            order.reload.update!
+          end
         end
       end
-    end
       
+    end
+        
+    
+      
+    config.to_prepare &method(:activate).to_proc
       
   end
 end
--- site/lib/site.rb
+++ site/lib/site.rb
@@ -10,6 +10,21 @@ module Site
       Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
         Rails.env.production? ? require(c) : load(c)
       end
+      
+      
+     ActiveSupport::Notifications.subscribe('spree.order.contents_changed') do |*args|
+        event = ActiveSupport::Notifications::Event.new(*args)
+        order = event.payload[:order]
+        order.line_items.each do |line_item|
+          if((order.state == "cart") && (line_item.quantity > line_item.variant.on_hand))
+            line_item.update_attribute(:quantity, line_item.variant.on_hand)
+            #order.update_totals && order.save
+            order.reload.update!
+          end
+        end
+      end
+      
+      
     end
     
     config.to_prepare &method(:activate).to_proc
