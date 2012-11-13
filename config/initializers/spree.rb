b9e47ea2af906ff93092f0ac0be1c75bcffca30b
Nikita Gnotovs <nxxn@inbox.lv>
2012-11-14 01:18:42 +0200

    ;

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -9,6 +9,15 @@ Spree.config do |config|
   # Example:
   # Uncomment to override the default site name.
   # config.site_name = "Spree Demo Site"
+  config.default_country_id = "110"
+  config.address_requires_state = "false"
+  config.admin_products_per_page = "5000"
+  config.orders_per_page = "5000"
+  config.admin_interface_logo = "admin/bg/spree_50.png"
+  config.logo = "admin/logo.png"
+  config.products_per_page = "16"
+
+
 end
 
 Spree.user_class = "Spree::User"
