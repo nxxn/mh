ecec960577dbcb138495678221edde861916fe0d
Nikita Gnotovs <nxxn@inbox.lv>
2012-12-21 01:58:56 +0200

    ;

--- db/migrate/20121030210001_remove_name_from_properties.rb
+++ db/migrate/20121030210001_remove_name_from_properties.rb
@@ -1,9 +1,9 @@
 class RemoveNameFromProperties < ActiveRecord::Migration
   def up
-    remove_column :spree_property_translations, :name
+  #  remove_column :spree_property_translations, :name
   end
 
   def down
-    add_column :spree_property_translations, :name, :string
+  #  add_column :spree_property_translations, :name, :string
   end
 end
