b671761cbdba6eaf6b17ec73f7ac17c41ef26077
Nikita Gnotov <n.gnotov@gmail.com>
2014-01-04 00:20:37 +0200

    merge

--- config/initializers/whatever.rb
+++
@@ -1,8 +0,0 @@
-if Spree::Config.instance
-Spree::Config.set(:default_locale => "ru")
-Spree::Config.set(:default_country_id=> "110")
-Spree::Config.set(:address_requires_state=> "false")
-Spree::Config.set(:admin_products_per_page=> "5000")
-Spree::Config.set(:show_zero_stock_products=> "false")
-
-end
